6bd5a215cba99c7ae93dc749694cba2e05d3776d
Bryan Abi Karam <bryancrisso@outlook.com>
2023-09-19 19:19:23 +0100

    Update dockerfile Include client script in docker

---
+++ docker/main.py
@@ -0,0 +1,22 @@
+import socket
+
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("Socket successfully created")
+except socket.error as err:
+    print("Socket creation failed with error %s" %(err))
+
+port = 6666
+
+host = socket.gethostname()
+
+s.connect((host, port))
+
+print("Successfully connected to host")
+
+s.send("12345\n".encode("utf-8"))
+print("Sent data")
+data = s.recv(1024).decode()
+print(data)
+
+s.close()
